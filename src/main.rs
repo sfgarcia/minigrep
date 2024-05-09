1b621ddaa0e014d90c988af661c423860c211881
sfgarcia <sfgarcia@uc.cl>
2024-05-09 18:46:32 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    let contents = std::fs::read_to_string(file_path)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents)
+}
