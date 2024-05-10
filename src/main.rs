f3fb2424c96faad676146b0eb420e1a66bac01e2
sfgarcia <sfgarcia@uc.cl>
2024-05-10 17:01:35 -0400

    Create lib file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use minigrep::{Config, run};
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,26 +13,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = std::fs::read_to_string(config.file_path)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents)
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
     }
 }
