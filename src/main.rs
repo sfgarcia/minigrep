04bc5915ddd1ab94158d00512c34b92c5c42acca
sfgarcia <sfgarcia@uc.cl>
2024-05-10 16:46:18 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,10 +23,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str>{
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
