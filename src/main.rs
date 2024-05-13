a6229516b3c1a397ea24a1c26d4d00579235663c
sfgarcia <sfgarcia@uc.cl>
2024-05-13 09:23:08 -0400

    Change error to stderr

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         std::process::exit(1);
     }
 }
