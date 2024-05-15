dd0c95547cefab9a43feacdb9a7fcbf144113748
sfgarcia <sfgarcia@uc.cl>
2024-05-14 20:46:51 -0400

    Cargo format

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::error::Error;
 use std::env::{self, Args};
+use std::error::Error;
 
 pub struct Config {
     pub query: String,
@@ -8,7 +8,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &mut Args) -> Result<Config, &'static str>{
+    pub fn build(args: &mut Args) -> Result<Config, &'static str> {
         args.next();
         let query = match args.next() {
             Some(arg) => arg,
@@ -20,24 +20,30 @@ impl Config {
         };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents.lines()
+    contents
+        .lines()
         .filter(|line| line.contains(query))
         .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    contents.lines()
+    contents
+        .lines()
         .filter(|line| line.to_lowercase().contains(&query))
         .collect()
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = std::fs::read_to_string(config.file_path)?;
 
     let results = if config.ignore_case {
@@ -78,6 +84,9 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+use minigrep::{run, Config};
 use std::env;
-use minigrep::{Config, run};
-
 
 fn main() {
     let config = Config::build(&mut env::args()).unwrap_or_else(|err| {
